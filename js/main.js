e8dab492b1232a267415777874b8f7e01d86e5c5
Danny Brown <xaerr00@gmail.com>
2024-11-27 17:32:23 -0800

    Building out services page

--- js/main.js
+++ js/main.js
@@ -75,33 +75,32 @@
         }
     });
 
-      // Service carousel
-      $(".service-carousel-peels").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        margin: 30,
-        loop: true,
-        dots: false,
-        nav: false,
-        responsive: {
-          0: {
-            items: 1
-          },
-          576: {
-            items: 1
-          },
-          768: {
-            items: 2
-          },
-          992: {
-            items: 2
-          },
-          1200: {
-            items: 2
-          }
-        }
-      });
-
+     // Service carousel
+$(".service-carousel-peels").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    loop: true,
+    dots: false,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      576: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 2
+      },
+      1200: {
+        items: 2
+      }
+    }
+  });
 
     // Pricing carousel
     $(".pricing-carousel").owlCarousel({
